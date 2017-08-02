1dbb30637f95639d8c910de810da1a7ebc531810
Will Furnell <mail@willfurnell.com>
2017-08-02 16:22:31 +0100

    Tries to fix photometry but

--- analysis/utils/photometry.py
+++ analysis/utils/photometry.py
@@ -13,7 +13,7 @@ def do_photometry(filename, file_id):
     """
     sex_command = ['sex', os.path.join(settings.FITS_DIRECTORY, filename), '-c',
                    os.path.join(settings.CONFIGS_DIRECTORY, 'default_phot.sex'), '-CATALOG_NAME',
-                   os.path.join(settings.CATALOGUE_ORIGINAL_DIRECTORY, filename) + ".cat", '-PARAMETERS_NAME',
+                   os.path.join(settings.CATALOGUE_ORIGINAL_DIRECTORY, filename + ".cat"), '-PARAMETERS_NAME',
                    os.path.join(settings.CONFIGS_DIRECTORY, 'default_phot.param'), '-FILTER_NAME',
                    os.path.join(settings.CONFIGS_DIRECTORY, 'default.conv')]
     subprocess.check_output(sex_command)
