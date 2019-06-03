019f61043f0cbb655f00f8b94e58025f27ee9b9f
Will Furnell <mail@willfurnell.com>
2019-06-03 09:53:30 +0100

    Up cpulimit to 25 mins

--- analysis/utils/astrometry.py
+++ analysis/utils/astrometry.py
@@ -95,7 +95,7 @@ def do_astrometry(path, file_id):
     # Actually solve the image using the astrometry.net solve-field command
     # We downsample and guess field to try and speed things up a bit. Don't go lower than 2 - causes problems
     solve_command = [settings.ASTROMETRY_BINARY_PATH + 'solve-field', 'in.fits', '--guess-scale',
-                     '--overwrite', '--cpulimit', '600', '--ra', str(rac), '--dec', str(decc), '--radius', '1.0']
+                     '--overwrite', '--cpulimit', '1500', '--ra', str(rac), '--dec', str(decc), '--radius', '1.0']
 
     try:
         subprocess.check_output(solve_command)
